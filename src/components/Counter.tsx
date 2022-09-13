1706d711bda3e1a9c88948e7f6218c0da2385d2c
Andrey Volkov <Andrey.Volkov.88@yandex.ru>
2022-09-13 22:30:39 +0300

    feat: add scss loader

---
+++ src/components/Counter.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './Counter.scss'
+
+export const Counter = () => {
+
+  const [count, setCount] = useState<number>(0)
+
+  const increment = () => {
+    setCount(() => count + 1)
+  }
+
+  const decrement = () => {
+    setCount(() => count - 1)
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button className="button" onClick={increment}>increment</button>
+      <button className="button" onClick={decrement}>decrement</button>
+    </div>
+  );
+};
