e26dd98b383471ee8e958c8766cab1c7aa97789d
Andrey Volkov <Andrey.Volkov.88@yandex.ru>
2022-09-14 13:02:51 +0300

    feat: add classNames

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import './styles/index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {useTheme} from "./theme/useTheme";
+import { classNames } from './helpers/classNames/classNames';
 
 const App = () => {
   const {theme, toggleTheme} = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames( 'app', { hovered: true, selected: false}, [theme, 'cls2'])}>
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
